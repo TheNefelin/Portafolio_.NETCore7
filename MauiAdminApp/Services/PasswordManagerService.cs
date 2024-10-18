7b1f97e7941f48e2844fb6365d06bb2a311b1840
Francisco Carmona <flcarmonac@yahoo.com>
2024-10-18 19:34:38 -0300

    Modify Request Api Query

--- MauiAdminApp/Services/PasswordManagerService.cs
+++ MauiAdminApp/Services/PasswordManagerService.cs
@@ -1,4 +1,5 @@
-﻿using ClassLibraryDTOs;
+﻿using Android.Media.TV;
+using ClassLibraryDTOs;
 using Newtonsoft.Json;
 using System.Net.Http.Headers;
 using System.Text;
@@ -8,36 +9,52 @@ namespace MauiAdminApp.Services
     public class PasswordManagerService
     {
         private readonly HttpClient _httpClient;
-
+        
         public PasswordManagerService(HttpClient httpClient)
         {
             _httpClient = httpClient;
         }
 
-        public async Task<ResponseApiDTO<List<CoreDTO>>> GetAll()
+        private async Task<ResponseApiDTO<T>> RequestApiQuery<T>(string uri, CoreDTO coreDTO)
         {
-            try
+            try 
             {
+                // se obtiene los datos de sesion del usuario
                 LoggedinDTO loggedinDTO = await AuthService.GetUser();
 
-                AddAuthorizationHeader(loggedinDTO.ApiToken);
+                if (coreDTO != null)
+                    coreDTO.Id_User = loggedinDTO.Id;
 
-                var response = await _httpClient.GetAsync($"/api/core/get-all/{loggedinDTO.SqlToken},{loggedinDTO.Id}");
+                // se prepara el body de la request
+                CoreRequestDTO<dynamic> coreRequestDTO = new()
+                {
+                    Sql_Token = loggedinDTO.SqlToken,
+                    Id_Usuario = loggedinDTO.Id,
+                    Id = coreDTO != null ? coreDTO.Id : null,
+                    CoreData = coreDTO,
+                };
+
+                // se prepara la cabecera de la request con el token de Bearer
+                if (!string.IsNullOrEmpty(loggedinDTO.ApiToken))
+                    _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", loggedinDTO.ApiToken);
+
+                var jsonContent = new StringContent(JsonConvert.SerializeObject(coreRequestDTO), Encoding.UTF8, "application/json");
+                var response = await _httpClient.PatchAsync(uri, jsonContent);
 
                 if (response.IsSuccessStatusCode)
                 {
                     var jsonResponse = await response.Content.ReadAsStringAsync();
-                    var objResponse = JsonConvert.DeserializeObject<ResponseApiDTO<List<CoreDTO>>>(jsonResponse);
+                    var objResponse = JsonConvert.DeserializeObject<ResponseApiDTO<T>>(jsonResponse);
                     return objResponse;
                 }
                 else
                 {
-                    // Manejo de respuesta cuando no es éxito
+                    // Manejo de respuesta cuando no es exitosa
                     var errorResponse = await response.Content.ReadAsStringAsync();
                     Console.WriteLine($"Error: {response.StatusCode} - {errorResponse}");
                 }
             }
-            catch (Exception ex) 
+            catch (Exception ex)
             {
                 Console.WriteLine($"Error en la obtencion de datos: {ex.Message}");
             }
@@ -45,90 +62,26 @@ namespace MauiAdminApp.Services
             return null;
         }
 
+        public async Task<ResponseApiDTO<List<CoreDTO>>> GetAll()
+        {
+            return await RequestApiQuery<List<CoreDTO>>($"/api/core/get-all", null);
+        }
+
         public async Task<ResponseApiDTO<CoreDTO>> Create(CoreDTO coreDTO)
         {
-            try
-            {
-                LoggedinDTO loggedinDTO = await AuthService.GetUser();
-                AddAuthorizationHeader(loggedinDTO.ApiToken);
-
-                coreDTO.Id = 0;
-                coreDTO.Id_User = loggedinDTO.Id;
-
-                var jsonContent = new StringContent(JsonConvert.SerializeObject(coreDTO), Encoding.UTF8, "application/json");
-                var response = await _httpClient.PostAsync($"/api/core/{loggedinDTO.SqlToken}", jsonContent);
-
-                if (response.IsSuccessStatusCode)
-                {
-                    var jsonResponse = await response.Content.ReadAsStringAsync();
-                    var objResponse = JsonConvert.DeserializeObject<ResponseApiDTO<CoreDTO>>(jsonResponse);
-                    return objResponse;
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error en la creacion de datos: {ex.Message}");
-            }
-
-            return null;
+            coreDTO.Id = 0;
+            return await RequestApiQuery<CoreDTO>($"/api/core/insert", coreDTO);
         }
 
         public async Task<ResponseApiDTO<CoreDTO>> Edit(CoreDTO coreDTO)
         {
-            try
-            {
-                LoggedinDTO loggedinDTO = await AuthService.GetUser();
-                AddAuthorizationHeader(loggedinDTO.ApiToken);
-
-                coreDTO.Id_User = loggedinDTO.Id;
-
-                var jsonContent = new StringContent(JsonConvert.SerializeObject(coreDTO), Encoding.UTF8, "application/json");
-                var response = await _httpClient.PutAsync($"/api/core/{loggedinDTO.SqlToken}", jsonContent);
-
-                if (response.IsSuccessStatusCode)
-                {
-                    var jsonResponse = await response.Content.ReadAsStringAsync();
-                    var objResponse = JsonConvert.DeserializeObject<ResponseApiDTO<CoreDTO>>(jsonResponse);
-                    return objResponse;
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error en la creacion de datos: {ex.Message}");
-            }
-
-            return null;
+            return await RequestApiQuery<CoreDTO>($"/api/core/update", coreDTO);
         }
 
         public async Task<ResponseApiDTO<CoreDTO>> Delete(int id)
         {
-            try {
-                LoggedinDTO loggedinDTO = await AuthService.GetUser();
-                AddAuthorizationHeader(loggedinDTO.ApiToken);
-
-                var response = await _httpClient.DeleteAsync($"/api/core/{loggedinDTO.SqlToken},{loggedinDTO.Id},{id}");
-
-                if (response.IsSuccessStatusCode)
-                {
-                    var jsonResponse = await response.Content.ReadAsStringAsync();
-                    var objResponse = JsonConvert.DeserializeObject<ResponseApiDTO<CoreDTO>>(jsonResponse);
-                    return objResponse;
-                }
-            }
-            catch (Exception ex) {
-                Console.WriteLine($"Error en la eliminacion de datos: {ex.Message}");
-            }
-
-            return null;
-        }
-
-        // Método para obtener el token almacenado y añadirlo en los headers
-        private void AddAuthorizationHeader(string apiToken)
-        {
-            if (!string.IsNullOrEmpty(apiToken))
-            {
-                _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiToken);
-            }
+            CoreDTO coreDTO = new() { Id = id };
+            return await RequestApiQuery<CoreDTO>($"/api/core/delete", coreDTO);
         }
     }
 }
